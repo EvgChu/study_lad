b5aec0057f0169b737f7a07000a8085e1b778473
KinChu <chuevgenii@gmail.com>
2024-05-18 21:24:16 +0300

    add simple example

---
+++ TEST_Horoscope/horoscopes/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from. import views
+
+urlpatterns = [
+    path('<int:sign_horoscope>/', views.get_info_about_sign_zodiac_by_number, name='zodiac_by_number'),
+    path('<sign_horoscope>/', views.get_info_about_sign_zodiac, name='horoscope_name'),
+]
