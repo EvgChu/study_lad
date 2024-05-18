7749a220ef341124c86b9b7460d3009d3d6031ae
KinChu <chuevgenii@gmail.com>
2024-05-18 09:40:10 +0300

    dont finish third lesson

--- thirdLessonBlog/bboard/urls.py
+++ thirdLessonBlog/bboard/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<int:rubric_id>/', views.rubric_bbs, name='rubric'),
     path('', views.index, name='index'),
 ]
--- thirdLessonBlog/samplesite/urls.py
+++ thirdLessonBlog/samplesite/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('bboard/', include('bboard.urls')),
+    path('/', include('bboard.urls'), name='bboard'),
 ]
