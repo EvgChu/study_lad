ae4a0a425b31a897fbac7bdf143444e95df2b37d
KinChu <chuevgenii@gmail.com>
2024-05-23 18:26:36 +0300

    proccesing form

--- 4_Lesson_First_Grud/first/views.py
+++ 4_Lesson_First_Grud/first/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from .forms import UserForm
 
 def index(request):
-    form = UserForm()
-    return render(request, 'first/index.html', {"form": form})
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        age = request.POST.get('age')
+        return HttpResponse(name + ' ' + age)
+    userform = UserForm()
+    return render(request, 'first/index.html', {"form": userform})
