6588539e09784a24e8b8a55b8668dd23c2391d5d
KinChu <chuevgenii@gmail.com>
2024-05-24 14:12:09 +0300

    test is_valid

--- 4_Lesson_First_Grud/first/forms.py
+++ 4_Lesson_First_Grud/first/forms.py
@@ -6,10 +6,14 @@ class UserForm(forms.Form):
         widget=forms.Textarea,
         initial="Дядя Ваня",
         help_text="Введите имя",
+        max_length=10,
+        min_length=2,
     )
     age = forms.IntegerField(
         label="Возраст",
         initial=18,
         help_text="Введите возраст",
+        max_value=100,
+        min_value=18,
     )
-    field_order = ['age', 'name']
+    ads = forms.BooleanField(label="Подтвердите", required=False)
--- 4_Lesson_First_Grud/first/views.py
+++ 4_Lesson_First_Grud/first/views.py
@@ -5,8 +5,12 @@ from .forms import UserForm
 
 def index(request):
     if request.method == 'POST':
-        name = request.POST.get('name')
-        age = request.POST.get('age')
-        return HttpResponse(name + ' ' + age)
+        userform = UserForm(request.POST)
+        if userform.is_valid():
+            return HttpResponse(
+                userform.changed_data['name'] + ' ' + userform.changed_data['name']
+            )
+        else:
+            return HttpResponse('Invalid form')
     userform = UserForm()
     return render(request, 'first/index.html', {"form": userform})
