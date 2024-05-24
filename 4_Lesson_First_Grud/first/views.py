2b3eba442c76518eb6800ba24b5e9b47cd77beb5
KinChu <chuevgenii@gmail.com>
2024-05-24 18:12:51 +0300

    print error in form

--- 4_Lesson_First_Grud/first/views.py
+++ 4_Lesson_First_Grud/first/views.py
@@ -4,13 +4,9 @@ from django.http import HttpResponse
 from .forms import UserForm
 
 def index(request):
+    userform = UserForm()
     if request.method == 'POST':
         userform = UserForm(request.POST)
         if userform.is_valid():
-            return HttpResponse(
-                userform.changed_data['name'] + ' ' + userform.changed_data['name']
-            )
-        else:
-            return HttpResponse('Invalid form')
-    userform = UserForm()
+            return HttpResponse(f'{userform.cleaned_data["name"]} {userform.cleaned_data["age"]}')
     return render(request, 'first/index.html', {"form": userform})
