8e165f0565804cee298b6d71f030c8ec1b9d6710
KinChu <chuevgenii@gmail.com>
2024-05-24 10:33:35 +0300

    tests forms

--- 4_Lesson_First_Grud/first/forms.py
+++ 4_Lesson_First_Grud/first/forms.py
@@ -1,5 +1,15 @@
 from django import forms
 
 class UserForm(forms.Form):
-    name = forms.CharField()
-    age = forms.IntegerField()
+    name = forms.CharField(
+        label="Имя", 
+        widget=forms.Textarea,
+        initial="Дядя Ваня",
+        help_text="Введите имя",
+    )
+    age = forms.IntegerField(
+        label="Возраст",
+        initial=18,
+        help_text="Введите возраст",
+    )
+    field_order = ['age', 'name']
