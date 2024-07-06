860fa2921834ccaa44072c1eacad84af412967af
KinChu <chuevgenii@gmail.com>
2024-07-06 23:15:06 +0300

    add boostrap to another page

--- 9_Lesson_TaskManager/task_manager_app/urls.py
+++ 9_Lesson_TaskManager/task_manager_app/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('task/<int:pk>/', views.task_detail, name='task_detail'),
     path('task/create/', views.task_create, name='task_create'),
-    path('task/<int:pk>/edit/', views.task_edit, name='task_edit'),
-    path('task/<int:pk>/delete/', views.task_delete, name='task_delete'),
+    path('task/edit/<int:pk>/', views.task_edit, name='task_edit'),
+    path('task/delete/<int:pk>/', views.task_delete, name='task_delete'),
 ]
